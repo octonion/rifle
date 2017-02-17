98eb2346fd3479f602497dfa06e49df06c5d174b
Christopher D. Long <galizur@gmail.com>
2017-02-17 15:42:51 -0500

    Team averages.

--- ncaa/sql/team_averages.sql
+++ ncaa/sql/team_averages.sql
@@ -15,3 +15,23 @@ where
 and ar_total>0 group by school) a
 on (a.school)=(s.school)
 order by total desc;
+
+copy (
+select s.school,s.sbt,a.art,(s.sbt+a.art) total
+from
+(select school,
+avg(sb_total)::numeric(5,2) as sbt
+from ncaa.individuals
+where
+    sb_squaded
+and sb_total>0 group by school) s
+join
+(select school,
+avg(ar_total)::numeric(5,2) as art
+from ncaa.individuals
+where
+    ar_squaded
+and ar_total>0 group by school) a
+on (a.school)=(s.school)
+order by total desc)
+to '/tmp/team_averages.csv' csv header;
